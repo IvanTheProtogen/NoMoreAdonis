2096c54910144378dc43e314afa4f184e4de298a
IvanTheScript <122665922+IvanTheProtogen@users.noreply.github.com>
2025-03-10 00:22:04 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,18 @@
--- better version 
+if getgenv().NoMoreAdonis then
+	return warn("NoMoreAdonis already loaded...")
+end
+getgenv().NoMoreAdonis = true
 
--- some versions of adonis anticheat requires handshake connections, this script doesn't support that. careful!
+game:GetService("StarterGui"):SetCore("DevConsoleVisible", true)
+
+print([[
+ _   _       __  __                   _       _             _     
+| \ | | ___ |  \/  | ___  _ __ ___   / \   __| | ___  _ __ (_)___ 
+|  \| |/ _ \| |\/| |/ _ \| '__/ _ \ / _ \ / _` |/ _ \| '_ \| / __|
+| |\  | (_) | |  | | (_) | | |  __// ___ \ (_| | (_) | | | | \__ \
+|_| \_|\___/|_|  |_|\___/|_|  \___/_/   \_\__,_|\___/|_| |_|_|___/
+
+~~~ By IvanTheSkid on GitHub ~~~]])
 
 local RS = game:GetService("ReplicatedStorage")
 local function GetAdonisRemote()
@@ -11,23 +23,69 @@ local function GetAdonisRemote()
 	end
 end
 
-local lp = game:GetService("Players").LocalPlayer
-local re = GetAdonisRemote()
-local rf = re.__FUNCTION
-
-local function infiniteyield()while task.wait((2^31)-1)do end end
-local function blank()end
-local function checkerror(...)task.spawn(error, ...)end
-
-local function main(old, inst)
-	return function(instt, ...)
-		if instt~=inst then
-			return old(inst, ...)
-		end
-		infiniteyield()
+-- this is for debugging
+local function stacky(tbl, idt)
+	if typeof(tbl)~="table" then return tbl end
+	idt = idt or ""
+	for i,v in tbl do
+		print(idt,i,"--",v,"(",typeof(v),")")
+		stacky(v,idt.."\t")
 	end
 end
 
-local old;old=hookfunction(lp.Kick, main(old, lp))
-local oldd;oldd=hookfunction(re.FireServer, main(oldd, re))
-local olddd;olddd=hookfunction(rf.InvokeServer, main(olddd, rf))
+local lp = game:GetService("Players").LocalPlayer
+local re = GetAdonisRemote()
+
+if not re then
+	warn("Adonis Anticheat not found, lucky!")
+end
+
+local learnedData
+
+local trigger;trigger = hookfunction(re.FireServer, function(inst,...)
+	if inst~=re then return trigger(inst,...) end
+	if not learnedData then
+		stacky({...})
+		warn("Data learned, stealing the job...")
+		learnedData = {...}
+	end
+end)
+
+warn("NoMoreAdonis activated, it is currently learning the behavior of the anti-cheat, please wait...")
+print("Don't turn on your RemoteSpy or anti-kick just yet!!!")
+
+local timeout = os.clock()+60
+repeat task.wait(1) until learnedData or os.clock()>timeout
+
+hookfunction(lp.Kick, function()end)
+local oldwarn;oldwarn = hookfunction(warn, function(...)
+	local dd = table.concat({...})
+	if not string.find(dd, "Kick is not a valid member") then
+		return oldwarn(...)
+	end
+end)
+
+if not learnedData then
+	return warn("Seems like there's no handshake, lucky!")
+end
+
+local main;function main(tbl)
+	if typeof(tbl)~="table" then return tbl end
+	for i,v in tbl do
+		if i == "Sent" then
+			tbl[i] = v+1
+		end
+		main(v)
+	end
+end
+
+task.spawn(function()
+	while true do
+		main(learnedData)
+		trigger(re, learnedData)
+		--stacky(learnedData)
+		task.wait(30)
+	end
+end)
+
+oldwarn("All setup! You can now safely use the RemoteSpy!")
