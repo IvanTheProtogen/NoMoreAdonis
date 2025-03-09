5c28b3e87cca114b0871af835640128717976d4a
IvanTheScript <122665922+IvanTheProtogen@users.noreply.github.com>
2025-03-09 16:08:35 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,16 +1,33 @@
--- Adonis Anticheat Bypass
+-- better version 
 
-warn("Finding and destroying the RemoteEvent...")
-for i,v in game:GetService("ReplicatedStorage"):GetChildren() do
-	if v:IsA("RemoteEvent") then
-		local aaa = string.split(v.Name,"")
-		if aaa[9]=="-" and aaa[14]=="-" and aaa[19]=="-" and aaa[24]=="-" then
-			v:Destroy()
-			print("Found and destroyed!")
+-- some versions of adonis anticheat requires handshake connections, this script doesn't support that. careful!
+
+local RS = game:GetService("ReplicatedStorage")
+local function GetAdonisRemote()
+	for i,v in RS:GetChildren() do
+		if v:IsA("RemoteEvent") and v:FindFirstChild("__FUNCTION") and v.__FUNCTION:IsA("RemoteFunction") then
+			return v
 		end
 	end
 end
 
-warn("Locking the Player.Kick function...")
-hookfunction(game.Players.LocalPlayer.Kick, function()repeat task.wait(2^31)until false end)
-print("Function locked!")
+local lp = game:GetService("Players").LocalPlayer
+local re = GetAdonisRemote()
+local rf = re.__FUNCTION
+
+local function infiniteyield()while task.wait((2^31)-1)do end end
+local function blank()end
+local function checkerror(...)task.spawn(error, ...)end
+
+local function main(old, inst)
+	return function(instt, ...)
+		if instt~=inst then
+			return old(inst, ...)
+		end
+		infiniteyield()
+	end
+end
+
+local old;old=hookfunction(lp.Kick, main(old, lp))
+local oldd;oldd=hookfunction(re.FireServer, main(oldd, re))
+local olddd;olddd=hookfunction(rf.InvokeServer, main(olddd, rf))
